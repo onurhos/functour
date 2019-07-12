7b89f66c3fc4777b4a33d8ff26a29ae22d9630e0
onur.hos <onurhos@gmail.com>
2019-07-13 00:24:01 +0300

    result module added.

---
+++ src/Functour.Types/Result.fs
@@ -0,0 +1,18 @@
+﻿namespace Types
+
+module Result =
+
+    let bind f result = 
+        match result with
+        | Ok success -> f success
+        | Error error -> Error error
+
+    let map f result =
+        match result with 
+        | Ok success -> Ok(f success)
+        | Error error -> Error error
+
+    let mapError f result =
+        match result with
+        | Ok success -> Ok success
+        | Error error -> Error(f error)
