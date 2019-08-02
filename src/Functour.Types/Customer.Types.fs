91383dd0791115a20d78d868203be336c053a889
onur.hos <onurhos@gmail.com>
2019-08-03 00:01:11 +0300

    customer & hotel types refactoring

--- src/Functour.Types/Customer.Types.fs
+++ src/Functour.Types/Customer.Types.fs
@@ -5,13 +5,29 @@ module CustomerTypes =
     open System
     open CommonTypes
 
+    type CustomerValidationError = 
+        | CustomerIdShouldNotBeEmpty
+
     type CustomerId = private CustomerId of Guid
     module CustomerId =
-        let create guid = if guid = Guid.Empty then Error "Guid should not be empty" else Ok (CustomerId guid)
+        let create guid = if guid = Guid.Empty then Error CustomerIdShouldNotBeEmpty else Ok (CustomerId guid)
         let value (CustomerId id) = id
 
-    type Customer = { 
-        Id: CustomerId; 
-        FirstName: Name50;
-        LastName: Name50;
+    type UnvalidatedCustomer = {
+        FirstName: string
+        MiddleName : string option
+        LastName: string
     }
+
+    type Customer = { 
+        Id: CustomerId
+        FirstName: Name50
+        MiddleName : Name50 option
+        LastName: Name50
+    }
+
+    type ValidateCustomer =
+        UnvalidatedCustomer -> Result<Customer, CustomerValidationError>
+
+    type SaveCustomer =
+        Customer -> unit
--- src/Functour.Types/Hotel.Types.fs
+++ src/Functour.Types/Hotel.Types.fs
@@ -5,23 +5,49 @@ module rec HotelTypes =
     open System
     open CommonTypes
 
+    //  Hotels
+
+    type HotelCreateError = 
+        | HotelIdShouldNotBeEmpty
+
     type HotelId = private HotelId of Guid
     module HotelId =
-        let create guid = if guid = Guid.Empty then Error "Guid should not be empty" else Ok (HotelId guid)
+        let create guid = if guid = Guid.Empty then Error HotelIdShouldNotBeEmpty else Ok (HotelId guid)
         let value (HotelId id) = id
 
     type Hotel = { 
-        Id: HotelId;
-        Name: Name100;
+        Id: HotelId
+        Name: Name100
         Concepts : HotelConcept list
+        Rooms : HotelRoom list
     }
+
+    //  Hotel Rooms
+
+    type HotelRoomCreateError = 
+        | HotelRoomIdShouldNotBeEmpty
+
+    type HotelRoomId = private HotelRoomId of Guid
+    module HotelRoomId =
+        let create guid = if guid = Guid.Empty then Error HotelRoomIdShouldNotBeEmpty else Ok (HotelRoomId guid)
+        let value (HotelRoomId id) = id
+
+    type HotelRoom = { 
+        Id: HotelRoomId
+        Name: Name100
+    }
+
+    //  Concepts
+
+    type HotelConceptCreateError = 
+        | HotelConceptIdShouldNotBeEmpty
 
     type HotelConceptId = private HotelConceptId of Guid
     module HotelConceptId =
-        let create guid = if guid = Guid.Empty then Error "Guid should not be empty" else Ok (HotelConceptId guid)
+        let create guid = if guid = Guid.Empty then Error HotelConceptIdShouldNotBeEmpty else Ok (HotelConceptId guid)
         let value (HotelConceptId id) = id
 
     type HotelConcept = { 
-        Id: HotelConceptId;
-        Name: Name50; 
+        Id: HotelConceptId
+        Name: Name100
     }
